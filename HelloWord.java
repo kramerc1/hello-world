8c093f8e1fb87c43b38dc9f4cbd94772955dd631
kramerc1 <54990182+kramerc1@users.noreply.github.com>
2019-09-06 08:59:53 -0500

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,15 @@
+
+/**
+ * My first BlueJ file this year
+ *
+ * @conman1161
+ * @version 9/6/2019
+ */
+public class HelloWord
+{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("I am a Java programmer!!!");
+        System.out.println(args[0]);
+    }
+}
